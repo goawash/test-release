9d74acb363fce995f4d2ca59e243f8c6c8ca8667
Alexei Talankov <alexei.talankov@neotech.by>
2020-04-16 19:57:11 +0300

    initial commit

---
+++ src/main/java/com/example/testrelease/TestReleaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.testrelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestReleaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestReleaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testrelease/TestReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testrelease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestReleaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
